d1a2cb54ee43c73faf82fa6dabbcaf0137c6c68c
Tim K <tkuester@users.noreply.github.com>
2019-10-22 18:28:36 -0400

    Add a git version, a good idea!
    This is a pretty simple trick, calling "git describe" and passing it
    as a compiler argument. I also added a few failsafes, just incase the
    project doesn't have any tags yet, or you're building this outside of
    git.
    
    The "--dirty" flag is helpful for keeping track of development work,
    and helps prevent uncommitted changes from sneaking out of your
    development machine!
    
    To override, you can call "make GIT_VERSION=1.2.3"

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
+GIT_VERSION := $(shell git describe --dirty || echo 0.0.0)
+
 all: pkgtut
 
 pkgtut: pkgtut.c
-	gcc -Wall -o $@ $^
+	gcc -Wall -DGIT_VERSION='"$(GIT_VERSION)"' -o $@ $^
 
 clean:
 	@rm -f pkgtut
--- pkgtut.c
+++ pkgtut.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+#ifndef GIT_VERSION
+#define GIT_VERSION "unversioned"
+#endif
+
 // Obligitory: this is bad code. Don't use scanf. It was written this way
 // to illustrate the debian packaging system and updates.
 
-int main(void) {
+int main(int argc, char **argv) {
     int greet_cnt;
+
+    printf("%s version %s\n", argv[0], GIT_VERSION);
 
     printf("How many times should I greet you? ");
     scanf("%d", &greet_cnt);
